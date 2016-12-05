5bf3c8915284d3b21f0aefae63fdc8df48b49cb3
Te Riu Warren <teriu@teriu.name>
2016-12-06 11:54:58 +1300

    Add first path of bin

---
+++ bin/front-end-scripts.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const spawn = require('cross-spawn');
+const script = process.argv[2];
+const args = process.argv.slice(3);
+
+console.log('Script:', script);
+
+switch (script) {
+case 'build':
+case 'eject':
+case 'start':
+case 'test':
+  const result = spawn.sync(
+    'node',
+    [require.resolve('../scripts/' + script)].concat(args),
+    {stdio: 'inherit'}
+  );
+  process.exit(result.status);
+  break;
+default:
+  console.log('Unknown script "' + script + '".');
+  console.log('Perhaps you need to update react-scripts?');
+  break;
+}
