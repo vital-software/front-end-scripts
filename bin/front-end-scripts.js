f772451c1116c6e6b17a7f3e53809085725f88dd
Te Riu Warren <teriu@teriu.name>
2016-12-06 12:00:22 +1300

    Make bin command executable

--- bin/front-end-scripts.js
+++ bin/front-end-scripts.js
@@ -1,24 +1,25 @@
-#!/usr/bin/env node
-const spawn = require('cross-spawn');
-const script = process.argv[2];
-const args = process.argv.slice(3);
+#! /usr/bin/env node
+
+// var spawn = require('cross-spawn');
+var script = process.argv[2];
+var args = process.argv.slice(3);
 
 console.log('Script:', script);
 
-switch (script) {
-case 'build':
-case 'eject':
-case 'start':
-case 'test':
-  const result = spawn.sync(
-    'node',
-    [require.resolve('../scripts/' + script)].concat(args),
-    {stdio: 'inherit'}
-  );
-  process.exit(result.status);
-  break;
-default:
-  console.log('Unknown script "' + script + '".');
-  console.log('Perhaps you need to update react-scripts?');
-  break;
-}
+// switch (script) {
+// case 'build':
+// case 'eject':
+// case 'start':
+// case 'test':
+//   const result = spawn.sync(
+//     'node',
+//     [require.resolve('../scripts/' + script)].concat(args),
+//     {stdio: 'inherit'}
+//   );
+//   process.exit(result.status);
+//   break;
+// default:
+//   console.log('Unknown script "' + script + '".');
+//   console.log('Perhaps you need to update react-scripts?');
+//   break;
+// }
