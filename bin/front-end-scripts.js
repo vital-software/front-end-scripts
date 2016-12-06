7aadb9609bf3bfe2bf750b0b4590b4a3b448a310
Te Riu Warren <teriu@teriu.name>
2016-12-06 13:53:41 +1300

    Add bin pass through config

--- bin/front-end-scripts.js
+++ bin/front-end-scripts.js
@@ -1,34 +1,23 @@
 #! /usr/bin/env node
 
-const shell = require('shelljs');
-
-// var spawn = require('cross-spawn');
+const spawn = require('cross-spawn');
 const script = process.argv[2];
-// const args = process.argv.slice(3);
-
-console.log('Script:', script);
+const args = process.argv.slice(3);
 
 switch (script) {
+    // case 'build':
+    // case 'eject':
+    // case 'test':
     case 'start':
-        shell.exec('webpack-dev-server');
+        const result = spawn.sync(
+            'node',
+            [require.resolve('../script/' + script)].concat(args),
+            { stdio: 'inherit' }
+        );
+        process.exit(result.status);
         break;
     default:
         console.log(`Unknown script ${script}.`);
         console.log('Perhaps you need to update front-end-scripts?');
         break;
 }
-
-// case 'build':
-// case 'eject':
-// case 'test':
-//   const result = spawn.sync(
-//     'node',
-//     [require.resolve('../scripts/' + script)].concat(args),
-//     {stdio: 'inherit'}
-//   );
-//   process.exit(result.status);
-//   break;
-// default:
-//   console.log('Unknown script "' + script + '".');
-//   console.log('Perhaps you need to update react-scripts?');
-//   break;
