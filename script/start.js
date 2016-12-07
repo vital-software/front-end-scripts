fe9339ab03409bf89dfbfa3c1018fa914b5f349d
Te Riu Warren <teriu@teriu.name>
2016-12-08 12:48:38 +1300

    Clean up webpack start script

--- script/start.js
+++ script/start.js
@@ -1,113 +1,50 @@
+/* eslint-disable no-console */
+
 const chalk = require('chalk');
 const clearConsole = require('../helper/clear-console');
-const config = require('../webpack.config.babel.js');
 const detect = require('detect-port');
-const formatWebpackMessages = require('../helper/format-webpack-messages');
 const getProcessForPort = require('../helper/get-process-for-port');
-const prompt = require('../helper/prompt');
+const options = require('../webpack.config.babel.js');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 
 const IS_INTERACTIVE = process.stdout.isTTY;
 const DEFAULTS = {
-    PORT: process.env.PORT || 3000, // eslint-disable-line
-    PROTOCOL: 'https'
+    HOST: options.defaults.host,
+    PORT: options.defaults.port,
+    PROTOCOL: options.defaults.protocol
 };
 
-let compiler;
+function run() {
+    let config = options.webpack(),
+        isFirstCompile = true;
 
-function setupCompiler(host, port) {
-    // "Compiler" is a low-level interface to Webpack.
-    // It lets us listen to some events and provide our own custom messages.
-    compiler = webpack(config());
+    const showInstructions = IS_INTERACTIVE || isFirstCompile;
 
-    // "invalid" event fires when you have changed a file, and Webpack is
-    // recompiling a bundle. WebpackDevServer takes care to pause serving the
-    // bundle, so if you refresh, it'll wait instead of serving the old one.
-    // "invalid" is short for "bundle invalidated", it doesn't imply any errors.
-    compiler.plugin('invalid', function() {
-        if (IS_INTERACTIVE) {
-            clearConsole();
-        }
-        console.log('Compiling...');
-    });
+    if (showInstructions) {
+        console.log();
+        console.log('The app is running at:');
+        console.log();
+        console.log('  ', chalk.cyan(`${DEFAULTS.PROTOCOL}://${DEFAULTS.HOST}:${DEFAULTS.PORT}/`));
+        console.log();
+        console.log('Note that the development build is not optimized.');
+        console.log('To create a production build, use', chalk.cyan('yarn run build'), '.');
+        console.log();
 
-    let isFirstCompile = true;
+        isFirstCompile = false;
+    }
 
-    // "done" event fires when Webpack has finished recompiling the bundle.
-    // Whether or not you have warnings or errors, you will get this event.
-    compiler.plugin('done', function(stats) {
-        if (IS_INTERACTIVE) {
-            clearConsole();
-        }
-
-        // We have switched off the default Webpack output in WebpackDevServer
-        // options so we are going to "massage" the warnings and errors and present
-        // them in a readable focused way.
-        let isSuccessful,
-            messages,
-            showInstructions;
-
-        messages = formatWebpackMessages(stats.toJson({}, true));
-        isSuccessful = !messages.errors.length && !messages.warnings.length;
-        showInstructions = isSuccessful && (IS_INTERACTIVE || isFirstCompile);
-
-        if (isSuccessful) {
-            console.log(chalk.green('Compiled successfully!'));
-        }
-
-        if (showInstructions) {
-            console.log();
-            console.log('The app is running at:');
-            console.log();
-            console.log('  ', chalk.cyan(`${DEFAULTS.PROTOCOL}://${host}:${port}/`));
-            console.log();
-            console.log('Note that the development build is not optimized.');
-            console.log('To create a production build, use', chalk.cyan('yarn run build'), '.');
-            console.log();
-
-            isFirstCompile = false;
-        }
-
-        // If errors exist, only show errors.
-        if (messages.errors.length) {
-            console.log(chalk.red('Failed to compile.'));
-            console.log();
-            messages.errors.forEach((message) => {
-                console.log(message);
-                console.log();
-            });
-            return;
-        }
-
-        // Show warnings if no errors were found.
-        if (messages.warnings.length) {
-            console.log(chalk.yellow('Compiled with warnings.'));
-            console.log();
-            messages.warnings.forEach((message) => {
-                console.log(message);
-                console.log();
-            });
-
-            // Teach some ESLint tricks.
-            console.log('You may use special comments to disable some warnings.');
-            console.log('Use', chalk.yellow('// eslint-disable-next-line'), 'to ignore the next line.');
-            console.log('Use', chalk.yellow('/* eslint-disable */'), 'to ignore all warnings in a file.');
-        }
-    });
-}
-
-function runDevServer(host, port) {
-    const devServer = new WebpackDevServer(compiler, config().devServer);
+    let compiler = webpack(config);
+    let devServer = new WebpackDevServer(compiler, options.webpack().devServer);
 
     // Launch WebpackDevServer
-    devServer.listen(port, (error) => {
+    devServer.listen(DEFAULTS.PORT, (error) => {
         if (error) {
             return console.log(error);
         }
 
         if (IS_INTERACTIVE) {
-            clearConsole();
+            // clearConsole();
         }
 
         console.log(chalk.cyan('Starting the development server...'));
@@ -115,18 +52,11 @@ function runDevServer(host, port) {
     });
 }
 
-function run(port) {
-    const host = process.env.HOST || 'localhost';
-
-    setupCompiler(host, port);
-    runDevServer(host, port);
-}
-
 // We attempt to use the default port but if it is busy, we offer the user to
 // run on a different port. `detect()` Promise resolves to the next free port.
 detect(DEFAULTS.PORT).then((port) => {
     if (port === DEFAULTS.PORT) {
-        run(port);
+        run();
         return;
     }
 
@@ -136,13 +66,7 @@ detect(DEFAULTS.PORT).then((port) => {
         let existingProcess = getProcessForPort(DEFAULTS.PORT);
         let question = chalk.yellow(`Something is already running on port ${DEFAULTS.PORT}.`,
             ((existingProcess) ? ' Probably:\n  ' + existingProcess : '')) +
-            '\n\nWould you like to run the app on another port instead?';
-
-        prompt(question, true).then((shouldChangePort) => {
-            if (shouldChangePort) {
-                run(port);
-            }
-        });
+            '\n\nTry clearing any processes running on that port and running the command again.';
     } else {
         console.log(chalk.red(`Something is already running on port ${DEFAULTS.PORT}.`));
     }
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,75 +1,93 @@
 /* eslint-disable filenames/match-regex */
 const path = require('path');
 
-const PATH_OUTPUT = '/asset/build/';
 
+// Paths
+const PATH_OUTPUT = '/asset/build/';
 const PATH_DESTINATION = path.join(__dirname, './public', PATH_OUTPUT);
 const PATH_SOURCE = path.join(__dirname, './client');
 const PATH_SOURCE_JS = `${PATH_SOURCE}/js`;
 const FILE_ENTRY = `${PATH_SOURCE_JS}/app.js`;
 
-const DEFAULT_OPTIONS = {
+
+// Options
+const HOST = 'localhost';
+const PORT = 3000;
+const PROTOCOL = 'https';
+const WEBPACK_DEFAULT_OPTIONS = {
     dev: true
 };
 
-module.exports = function(options = DEFAULT_OPTIONS) {
-    const { dev } = options;
 
-    return {
-        // devtool: dev ? 'cheap-module-eval-source-map' : 'hidden-source-map',
+// Webpack config
+module.exports = {
+    defaults: {
+        host: HOST,
+        port: PORT,
+        protocol: PROTOCOL
+    },
+    webpack: function(options = WEBPACK_DEFAULT_OPTIONS) {
+        const { dev } = options;
 
-        entry: {
-            main: FILE_ENTRY
-        },
+        return {
+            // devtool: dev ? 'cheap-module-eval-source-map' : 'hidden-source-map',
 
-        output: {
-            path: PATH_DESTINATION,
-            filename: '[name].js'
-        },
+            entry: {
+                main: [
+                    `webpack-dev-server/client?${PROTOCOL}://${HOST}:${PORT}/`,
+                    FILE_ENTRY
+                ]
+            },
 
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: 'babel-loader'
-                },
-            ]
-        },
+            output: {
+                path: PATH_DESTINATION,
+                filename: '[name].js'
+            },
 
-        resolve: {
-            modules: [
-                PATH_SOURCE_JS,
-                'node_modules'
-            ],
-        },
+            module: {
+                rules: [
+                    {
+                        test: /\.(js|jsx)$/,
+                        exclude: /node_modules/,
+                        use: 'babel-loader'
+                    },
+                ]
+            },
 
-        devServer: {
-            // Add GZip compression
-            compress: true,
+            resolve: {
+                modules: [
+                    PATH_SOURCE_JS,
+                    'node_modules'
+                ],
+            },
 
-            // Use /public/ as the default content base
-            contentBase: path.join(__dirname, 'public'),
+            devServer: {
+                // Add GZip compression
+                compress: true,
 
-            // index.html will catch all routes (allowing Router to do it's thing)
-            historyApiFallback: true,
+                // Use /public/ as the default content base
+                contentBase: path.join(__dirname, 'public'),
 
-            // Hot module replacement
-            // hot: true,
+                // index.html will catch all routes (allowing Router to do it's thing)
+                historyApiFallback: true,
 
-            // Enable HTTPS and HTTP/2
-            https: true,
+                // Hot module replacement
+                // hot: true,
 
-            // Hide the webpack bundle information
-            // noInfo: true,
+                // Enable HTTPS and HTTP/2
+                https: true,
 
-            // Match public path with output path
-            publicPath: PATH_OUTPUT,
+                // Hide the webpack bundle information
+                // noInfo: true,
 
-            watchOptions: {
-                // Don't actively watch the node_modules folder to decrease CPU usage
-                ignored: /node_modules/
+                // Match public path with output path
+                publicPath: PATH_OUTPUT,
+
+                watchOptions: {
+                    // Don't actively watch the node_modules folder to decrease CPU usage
+                    ignored: /node_modules/
+                }
             }
-        }
-    };
+        };
+    }
 };
