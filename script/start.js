d163a7be8656b40e9ccf995b5986ec64916e5400
Te Riu Warren <teriu@teriu.name>
2016-12-08 16:17:16 +1300

    Add better webpack console logging

--- script/start.js
+++ script/start.js
@@ -38,7 +38,12 @@ function run() {
     }
 
     compiler.plugin('invalid', function() {
-        console.log('Compiling...');
+        if (IS_INTERACTIVE) {
+            // clearConsole();
+        }
+
+        console.log('');
+        console.log(chalk.blue('Compiling...'));
     });
 
     compiler.plugin('done', function(stats) {
@@ -49,8 +54,38 @@ function run() {
         // We have switched off the default Webpack output in WebpackDevServer
         // options so we are going to "massage" the warnings and errors and present
         // them in a readable focused way.
-        const messages = stats.toString('minimal', true);
-        console.log(messages);
+        const messages = stats.toJson('minimal', true);
+        const isSuccessful = !messages.errors.length && !messages.warnings.length;
+
+        if (isSuccessful) {
+            console.log(chalk.green('Compiled successfully!'));
+        }
+
+        // If errors exist, only show errors.
+        if (messages.errors.length) {
+            console.log(chalk.red('Failed to compile.'));
+            console.log();
+            messages.errors.forEach((message) => {
+                console.log(message);
+                console.log();
+            });
+            return;
+        }
+
+        // Show warnings if no errors were found.
+        if (messages.warnings.length) {
+            console.log(chalk.yellow('Compiled with warnings.'));
+            console.log();
+            messages.warnings.forEach((message) => {
+                console.log(message);
+                console.log();
+            });
+
+            // Teach some ESLint tricks.
+            console.log('You may use special comments to disable some warnings.');
+            console.log('Use', chalk.yellow('// eslint-disable-next-line'), 'to ignore the next line.');
+            console.log('Use', chalk.yellow('/* eslint-disable */'), 'to ignore all warnings in a file.');
+        }
     });
 
     // Launch WebpackDevServer
