92a76a3bf8880607bbaad8f0568eab58965f7e30
Te Riu Warren <teriu@teriu.name>
2017-07-03 12:56:07 +1200

    Update naming

--- helper/get-process-for-port.js
+++ helper/get-process-for-port.js
@@ -1,6 +1,6 @@
-const chalk = require('chalk');
-const execSync = require('child_process').execSync;
-const path = require('path');
+const chalk = require('chalk')
+const execSync = require('child_process').execSync
+const path = require('path')
 
 let execOptions = {
     encoding: 'utf8',
@@ -9,55 +9,59 @@ let execOptions = {
         'pipe', // stdout (default)
         'ignore' // stderr
     ]
-};
+}
 
 function isProcessAReactApp(processCommand) {
-    return (/^node .*front-end-scripts\/scripts\/start\.js\s?$/).test(processCommand);
+    return (/^node .*vitalizer\/scripts\/start\.js\s?$/).test(processCommand)
 }
 
 function getProcessIdOnPort(port) {
-    return execSync(`lsof -i:${port} -P -t -sTCP:LISTEN`, execOptions).trim();
+    return execSync(`lsof -i:${port} -P -t -sTCP:LISTEN`, execOptions).trim()
 }
 
 function getPackageNameInDirectory(directory) {
-    let packagePath = path.join(directory.trim(), 'package.json');
+    let packagePath = path.join(directory.trim(), 'package.json')
 
     try {
-        return require(packagePath).name;
+        return require(packagePath).name
     } catch (e) {
-        return null;
+        return null
     }
 }
 
 function getProcessCommand(processId, processDirectory) {
-    let command = execSync(`ps -o command -p ${processId} | sed -n 2p`, execOptions);
+    let command = execSync(
+        `ps -o command -p ${processId} | sed -n 2p`,
+        execOptions
+    )
 
     if (isProcessAReactApp(command)) {
-        const packageName = getPackageNameInDirectory(processDirectory);
-        return (packageName) ? `${packageName}\n` : command;
+        const packageName = getPackageNameInDirectory(processDirectory)
+        return packageName ? `${packageName}\n` : command
     }
 
-    return command;
+    return command
 }
 
 function getDirectoryOfProcessById(processId) {
-    return execSync(`lsof -p ${processId} | grep cwd | awk '{print $9}'`, execOptions).trim();
+    return execSync(
+        `lsof -p ${processId} | grep cwd | awk '{print $9}'`,
+        execOptions
+    ).trim()
 }
 
 function getProcessForPort(port) {
     try {
-        let command,
-            directory,
-            processId;
+        let command, directory, processId
 
-        processId = getProcessIdOnPort(port);
-        directory = getDirectoryOfProcessById(processId);
-        command = getProcessCommand(processId, directory);
+        processId = getProcessIdOnPort(port)
+        directory = getDirectoryOfProcessById(processId)
+        command = getProcessCommand(processId, directory)
 
-        return chalk.cyan(command) + chalk.blue('  in ') + chalk.cyan(directory);
+        return chalk.cyan(command) + chalk.blue('  in ') + chalk.cyan(directory)
     } catch (e) {
-        return null;
+        return null
     }
 }
 
-module.exports = getProcessForPort;
+module.exports = getProcessForPort
