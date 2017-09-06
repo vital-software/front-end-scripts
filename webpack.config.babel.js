7615cba085b768a3e04928fc5bfab4603fa30bb7
Te Riu Warren <teriu@teriu.name>
2017-09-07 04:39:18 +1200

    Add GraphQL loader

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -222,7 +222,7 @@ module.exports = {
                     {
                         test: /\.(graphql|gql)$/,
                         exclude: /node_modules/,
-                        loader: 'raw-loader'
+                        loader: 'graphql-tag/loader'
                     },
                     {
                         test: /\.(js|jsx)$/,
