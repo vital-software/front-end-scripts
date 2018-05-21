d5533f10cd9f54e4e693d42478a9b851ef0f1019
Hal Smith Stevens <hal@vitaler.co>
2018-05-21 13:14:56 +1200

    Remove explicit entry urls

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -83,9 +83,9 @@ module.exports = {
 
         if (isDev) {
             indexEntry.unshift(
-                `${paths.ownNodeModules}/react-hot-loader/patch`,
-                `${paths.ownNodeModules}/webpack-dev-server/client?${PROTOCOL}://${HOST}/`,
-                `${paths.ownNodeModules}/webpack/hot/only-dev-server`
+                'react-hot-loader/patch',
+                `webpack-dev-server/client?${PROTOCOL}://${HOST}/`,
+                'webpack/hot/only-dev-server'
             )
         }
         indexEntry.push(paths.appIndexJs)
