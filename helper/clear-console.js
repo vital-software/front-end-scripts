fbf2497ee1c419ad5836ad8cc2a6fe7f01de8ae6
Te Riu Warren <te@vitaler.co>
2018-06-27 10:28:46 +1200

    Clean up clear-console

--- helper/clear-console.js
+++ helper/clear-console.js
@@ -1,10 +1,5 @@
-let isFirstClear = true
-
 function clearConsole() {
-    // On first run, clear completely so it doesn't show half screen on Windows.
-    // On next runs, use a different sequence that properly scrolls back.
-    process.stdout.write(isFirstClear ? '\x1bc' : '\x1b[2J\x1b[0f')
-    isFirstClear = false
+    process.stdout.write(process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H')
 }
 
 module.exports = clearConsole
