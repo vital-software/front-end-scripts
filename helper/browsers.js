a33da0a910ac66501112c60122eff27db5be3ac2
Te Riu Warren <te@vitaler.co>
2018-06-27 14:10:55 +1200

    Fix lint issues in browsers file

--- helper/browsers.js
+++ helper/browsers.js
@@ -1,7 +1,6 @@
 const browserslist = require('browserslist')
 const chalk = require('chalk')
 const os = require('os')
-const fs = require('fs')
 
 const defaultBrowsers = {
     development: ['chrome', 'firefox', 'edge'].map((browser) => `last 2 ${browser} versions`),
@@ -11,16 +10,16 @@ const defaultBrowsers = {
 function checkBrowsers(dir, retry = true) {
     const current = browserslist.findConfig(dir)
 
-    if (current != null) {
+    if (current !== null) {
         return Promise.resolve(current)
     }
 
     if (!retry) {
         return Promise.reject(
             new Error(
-                `${chalk.red('You must specify targeted browsers.') +
-                    os.EOL
-                }Please add a ${chalk.bold('.browserslistrc')} file to the project.`
+                `${chalk.red('You must specify targeted browsers.') + os.EOL}Please add a ${chalk.bold(
+                    '.browserslistrc'
+                )} file to the project.`
             )
         )
     }
@@ -30,16 +29,16 @@ function checkBrowsers(dir, retry = true) {
 
 function printBrowsers(dir) {
     return checkBrowsers(dir).then((browsers) => {
-        if (browsers == null) {
+        if (browsers === null) {
             console.log('Built the bundle with default browser support.')
 
             return
         }
-        browsers = browsers[process.env.NODE_ENV] || browsers
-        if (Array.isArray(browsers)) {
-            browsers = browsers.join(', ')
-        }
-        console.log(`Built the bundle with browser support for ${chalk.cyan(browsers)}.`)
+
+        const browsersList = browsers[process.env.NODE_ENV] || browsers
+        const output = Array.isArray(browsersList) ? browsersList.join(', ') : browsersList
+
+        console.log(`Built the bundle with browser support for ${chalk.cyan(output)}.`)
     })
 }
 
