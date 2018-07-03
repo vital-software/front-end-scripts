933f3a606149a9495d81b228e9f5a9abc54286b7
Te Riu Warren <te@vitaler.co>
2018-07-03 12:23:41 +1200

    Fix browser file error

--- helper/browsers.js
+++ helper/browsers.js
@@ -2,15 +2,10 @@ const browserslist = require('browserslist')
 const chalk = require('chalk')
 const os = require('os')
 
-const defaultBrowsers = {
-    development: ['chrome', 'firefox', 'edge'].map((browser) => `last 2 ${browser} versions`),
-    production: ['>0.25%', 'not op_mini all', 'ie 11']
-}
-
 function checkBrowsers(dir, retry = true) {
     const current = browserslist.findConfig(dir)
 
-    if (current !== null) {
+    if (current) {
         return Promise.resolve(current)
     }
 
@@ -42,4 +37,4 @@ function printBrowsers(dir) {
     })
 }
 
-module.exports = { defaultBrowsers, checkBrowsers, printBrowsers }
+module.exports = { checkBrowsers, printBrowsers }
