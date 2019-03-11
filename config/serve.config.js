fdefb67f6ed7ab67c5f26911c99d95ff702567c8
Hal Smith Stevens <hal@vitaler.co>
2019-03-12 11:12:23 +1300

    Added config

---
+++ config/serve.config.js
@@ -0,0 +1,22 @@
+const paths = require('../config/paths')
+
+module.exports = {
+    // Use gzip compression
+    compress: true,
+    // Use /static/ as the default content base
+    contentBase: paths.appPublic,
+    // Allow connection to HMR websocket
+    disableHostCheck: true,
+    // index.html will catch all routes (allowing Router to do it's thing)
+    historyApiFallback: true,
+    // Hot module replacement
+    hot: true,
+    // Allow serving externally
+    host: '0.0.0.0',
+    // Disable HTTPS and HTTP/2 - Traefik proxy terminates SSL
+    https: false,
+    // Hide the webpack bundle information
+    noInfo: true,
+    // Match public path with output path
+    publicPath: '/'
+}
