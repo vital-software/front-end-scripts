675cd6cf3bed512d1dd46c8374b0b2b0da0392b2
Te Riu Warren <te@vitaler.com>
2019-12-16 15:00:13 +1300

    Try CRA config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -77,61 +77,42 @@ module.exports = smp.wrap({
             // JavaScript minfier
             new TerserPlugin({
                 terserOptions: {
-                    // Keep classnames & function names for react component stack traces
-                    keep_classnames: true,
-                    keep_fnames: true,
-
-                    // Work around the Safari 10 loop iterator bug.
-                    safari10: true,
-                    
                     parse: {
-                        // we want terser to parse ecma 8 code. However, we don't want it
-                        // to apply any minfication steps that turns valid ecma 5 code
+                        // We want terser to parse ecma 8 code. However, we don't want it
+                        // to apply any minification steps that turns valid ecma 5 code
                         // into invalid ecma 5 code. This is why the 'compress' and 'output'
                         // sections only apply transformations that are ecma 5 safe
                         // https://github.com/facebook/create-react-app/pull/4234
-                        ecma: 8
+                        ecma: 8,
                     },
                     compress: {
-                        // Disabled because of an issue with Terser breaking valid code:
-                        // https://github.com/facebook/create-react-app/issues/5250
-                        // Pending futher investigation:
-                        // https://github.com/terser-js/terser/issues/120
-                        inline: 2,
-
                         ecma: 5,
-
                         warnings: false,
-
                         // Disabled because of an issue with Uglify breaking seemingly valid code:
                         // https://github.com/facebook/create-react-app/issues/2376
                         // Pending further investigation:
                         // https://github.com/mishoo/UglifyJS2/issues/2011
-                        comparisons: false
+                        comparisons: false,
+                        // Disabled because of an issue with Terser breaking valid code:
+                        // https://github.com/facebook/create-react-app/issues/5250
+                        // Pending further investigation:
+                        // https://github.com/terser-js/terser/issues/120
+                        inline: 2,
                     },
+                    mangle: {
+                        safari10: true,
+                    },
+                    // Keep classnames & function names for react component stack traces
+                    keep_classnames: true,
+                    keep_fnames: true,
                     output: {
                         ecma: 5,
-
                         comments: false,
-
                         // Turned on because emoji and regex is not minified properly using default
                         // https://github.com/facebook/create-react-app/issues/2488
-                        ascii_only: true
-                    }
+                        ascii_only: true,
+                    },
                 },
-
-                // Extract comments (i.e. licenses) to a separate file.
-                // https://github.com/webpack/webpack/commit/71933e979e51c533b432658d5e37917f9e71595a
-                extractComments: true,
-
-                // Use multi-process parallel running to improve the build speed
-                // Default number of concurrent runs: os.cpus().length - 1
-                parallel: true,
-
-                // Enable file caching
-                cache: true,
-
-                // Use source maps to map error message locations to module
                 sourceMap: true
             }),
 
