5430cfbf94f1a9fd82bfb6a930acdeee89d0491d
Te Riu Warren <te@vitaler.com>
2019-12-16 15:21:39 +1300

    Update Terser Options

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -77,42 +77,17 @@ module.exports = smp.wrap({
             // JavaScript minfier
             new TerserPlugin({
                 terserOptions: {
-                    parse: {
-                        // We want terser to parse ecma 8 code. However, we don't want it
-                        // to apply any minification steps that turns valid ecma 5 code
-                        // into invalid ecma 5 code. This is why the 'compress' and 'output'
-                        // sections only apply transformations that are ecma 5 safe
-                        // https://github.com/facebook/create-react-app/pull/4234
-                        ecma: 8,
-                    },
-                    compress: {
-                        ecma: 5,
-                        warnings: false,
-                        // Disabled because of an issue with Uglify breaking seemingly valid code:
-                        // https://github.com/facebook/create-react-app/issues/2376
-                        // Pending further investigation:
-                        // https://github.com/mishoo/UglifyJS2/issues/2011
-                        comparisons: false,
-                        // Disabled because of an issue with Terser breaking valid code:
-                        // https://github.com/facebook/create-react-app/issues/5250
-                        // Pending further investigation:
-                        // https://github.com/terser-js/terser/issues/120
-                        inline: 2,
-                    },
-                    mangle: {
-                        safari10: true,
-                    },
-                    // Keep classnames & function names for react component stack traces
+                    parse: {},
+                    compress: {},
+                    mangle: true,
+                    // Keep classnames & function names for React Component stack traces
                     keep_classnames: true,
                     keep_fnames: true,
                     output: {
-                        ecma: 5,
-                        comments: false,
-                        // Turned on because emoji and regex is not minified properly using default
-                        // https://github.com/facebook/create-react-app/issues/2488
-                        ascii_only: true,
+                        comments: false
                     },
                 },
+                safari10: true,
                 sourceMap: true
             }),
 
